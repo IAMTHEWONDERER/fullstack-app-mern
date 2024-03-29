0651f52400e5e4ec098a2f03a3fd7a673695cadf
Oussama Alouche <theoblivionitself@gmail.com>
2024-03-29 23:59:40 +0000

    more models + new routes

--- api/server.js
+++ api/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const mongoose = require ('mongoose');
+import userRoute from './routes/userRoute.js'
 const app = express();
 dotenv.config();
+
+
+
+
 mongoose.set("strictQuery", true);
+
 
 const connect = async()=>{
     try{
@@ -13,4 +19,7 @@ const connect = async()=>{
         res.status(500).send({message: err.message});
     }
 };
+
+
+app.use("api/user", userRoute);
 app.listen(5656,()=>{console.log("backend server is running")})
